c5cc07f6e966c35e7f4adaf79d54759406644ea7
trainer2edureka <trainer2edureka@gmail.com>
2020-08-28 06:49:41 +0000

    "committed"

--- Demo.java
+++ Demo.java
@@ -4,7 +4,8 @@ public class Demo
 	public static void main(String[] args)
 		{
 			System.out.println("My first Java File");
-			System.out.println("Some more content");
+			System.out.println("Some more content edited in Master file ");
+			System.out.println("Some more content edited in Branch file ");
 
 		}
 
